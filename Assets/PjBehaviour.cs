d4d7ace5db775282a398f7899af24768c86b9e97
Mognom <alex.mognom@gmail.com>
2015-03-24 23:32:38 +0100

    Death collision and platforms
    Added death and platforms.
    Fixed jump while falling.
    Tweaked speed and collision code event changes.

--- Assets/PjBehaviour.cs
+++ Assets/PjBehaviour.cs
@@ -2,40 +2,41 @@
 using System.Collections;
 
 public class PjBehaviour : MonoBehaviour {
-	Vector2 jumpVector, moveVect;
-	Rigidbody2D rb;
-	ushort grounded;
+	private Vector2 jumpVector, moveVect;
+	private Rigidbody2D rb;
+	private ushort grounded;
+
 	// Use this for initialization
 	void Start () {
 		rb = this.gameObject.GetComponent<Rigidbody2D> ();
 		grounded = 0;
 		jumpVector = new Vector2 (0, 1);
 		moveVect = new Vector2 (1, 0);
-		rb.AddForce(moveVect * 70);
+		rb.velocity = moveVect*5;
+		//rb.AddForce(moveVect * 70);
 
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		//rb.AddForce(moveVect * 1);
+
+		if (rb.velocity.magnitude < 3) {
+			print ("why dead?");
+			print (rb.velocity.magnitude);
+		}
 		if (Input.GetKey (KeyCode.W) && grounded >0 ) {
-			rb.AddForce (jumpVector * 120);
-			grounded--;
+			rb.AddForce (jumpVector * 10, ForceMode2D.Impulse);
+			grounded = 0;
 		}
 	}
 
-	void OnCollisionStay2D(Collision2D coll){
 
-		switch (coll.gameObject.tag) {
-		case "ground":
-			grounded  = 1;
-			break;
-		case "doom":
-			print("tasmuerto");
-			break;
-		default:
-			break;
-		}
+	void OnCollisionEnter2D(Collision2D coll){
+		grounded  = 1;
+	}
+
+	void OnCollisionExit2D(Collision2D coll){
+		grounded = 0;
 	}
 
 }
