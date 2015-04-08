c67cb215fac3fd2cac0397f4730271d1545c8a06
Mognom <alex.mognom@gmail.com>
2015-04-08 19:54:06 +0200

    EndFlag

---
+++ Assets/SceneControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneControl : MonoBehaviour {
+	public PjBehaviour pj;
+	private Vector3 initialPos;
+
+	// Use this for initialization
+	void Start () {
+		initialPos = pj.transform.position;
+	}
+	
+
+	IEnumerator deathTrigger(){
+		//Start death animation
+		print ("Muerte y destruccion");
+		yield return new WaitForSeconds (3.0f);
+		resetScene ();
+	}
+
+	private void resetScene(){
+		pj.transform.position = initialPos;
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		print ("entra");
+		StartCoroutine(deathTrigger ());
+		print ("sale");
+	}
+
+}
